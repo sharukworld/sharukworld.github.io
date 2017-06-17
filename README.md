40c7a5e85e056aa8e66e64adbdec283be5efa65a
sharuk ahmed mazumder <sharuk.ahmed@coda.global>
2017-06-17 21:33:52 +0530

    fixed empty cookie

--- README.md
+++
@@ -1,1 +0,0 @@
-# sharukworld.github.io
--- jira-shark/src/app/app.component.ts
+++ jira-shark/src/app/app.component.ts
@@ -18,16 +18,16 @@ export class AppComponent {
   }
   url;
   password: string = '';
-  
-  
+
+
   fromDate: string = '';
-  userDataObj:any = {
-      username: '',
-      jiraDomain:'',
-      project: '',
-      fromDate: '',
-      toDate: ''
-    }
+  userDataObj: any = {
+    username: '',
+    jiraDomain: '',
+    project: '',
+    fromDate: '',
+    toDate: ''
+  }
   toDate: string;
   userList: UserModel[] = [];
   issues = [];
@@ -36,7 +36,9 @@ export class AppComponent {
   ngOnInit() {
     this.today.setHours(0, 0, 0, 0);
     this.todayDate = (this.today).toLocaleString('en-GB').slice(0, 10).split("\/").reverse().join("-");
-    this.userDataObj = this.cookieService.getObject('jirKey');
+    let cookies = this.cookieService.getObject('jirKey');
+    if (cookies != null)
+      this.userDataObj = cookies;
   }
   // getAllUser() {
   //   let url = this.jiraDomain + Url.user + this.project;
@@ -62,59 +64,59 @@ export class AppComponent {
   moreThan20Logs = [];
   getWorkLogFromTo() {
     this.setCookies();
-   this.userList = [];
-     let queryDate = (this.userDataObj.fromDate === '') ? this.todayDate : this.userDataObj.fromDate;
-     this.userDataObj.fromDate = queryDate;
-     let url = "http://jira-oc-services-jira-services.7e14.starter-us-west-2.openshiftapps.com/worklogs";
-     this.httpService.securePost(url, this.userDataObj,  this.userDataObj.username, this.password).subscribe(
-       res => {
-         console.log('res', res.json());
-         this.userList = res.json();
-       }
-     );
-  //   let url = this.jiraDomain + Url.allTicketWorkLog + '&fields=key,summary,worklog&jql=project= ' + this.project + ' and updated >' + queryDate;
-  //   this.httpService.secureGet(url, this.username, this.password).subscribe(
-  //     res => {
-  //       this.issues = res.json().issues;
-  //       let lessThan21Logs = [];
-  //       this.issues.forEach(el => {
-  //         if (el.fields.worklog.total > 20) {
-  //           this.moreThan20Logs.push(el);
-  //         }
-  //         else {
-  //           lessThan21Logs.push(el);
-  //         }
-  //       });
-  //       console.log('issues', this.issues);
-  //       if (this.moreThan20Logs.length !== 0) {
-  //         console.log('morethan20', this.moreThan20Logs);
+    this.userList = [];
+    let queryDate = (this.userDataObj.fromDate === '') ? this.todayDate : this.userDataObj.fromDate;
+    this.userDataObj.fromDate = queryDate;
+    let url = "http://jira-oc-services-jira-services.7e14.starter-us-west-2.openshiftapps.com/worklogs";
+    this.httpService.securePost(url, this.userDataObj, this.userDataObj.username, this.password).subscribe(
+      res => {
+        console.log('res', res.json());
+        this.userList = res.json();
+      }
+    );
+    //   let url = this.jiraDomain + Url.allTicketWorkLog + '&fields=key,summary,worklog&jql=project= ' + this.project + ' and updated >' + queryDate;
+    //   this.httpService.secureGet(url, this.username, this.password).subscribe(
+    //     res => {
+    //       this.issues = res.json().issues;
+    //       let lessThan21Logs = [];
+    //       this.issues.forEach(el => {
+    //         if (el.fields.worklog.total > 20) {
+    //           this.moreThan20Logs.push(el);
+    //         }
+    //         else {
+    //           lessThan21Logs.push(el);
+    //         }
+    //       });
+    //       console.log('issues', this.issues);
+    //       if (this.moreThan20Logs.length !== 0) {
+    //         console.log('morethan20', this.moreThan20Logs);
 
-          // for (let i = 0; i < this.moreThan20Logs.length; i++) {
-          //   console.log('for', this.moreThan20Logs[i]);
-          //   this.getIssueIndetail(this.moreThan20Logs[i].key).subscribe(
-          //     res => {
-          //       this.moreThan20Logs[i].fields.worklog = res.json();
-          //       if (i === this.moreThan20Logs.length - 1) {
-          //         this.issues = [];
-          //         this.issues = this.issues.concat(this.moreThan20Logs).concat(lessThan21Logs);
-          //         console.log('issues', this.issues);
-          //         this.mapUserAndIssue(this.issues);
-          //       }
-          //     }
-          //   );
+    // for (let i = 0; i < this.moreThan20Logs.length; i++) {
+    //   console.log('for', this.moreThan20Logs[i]);
+    //   this.getIssueIndetail(this.moreThan20Logs[i].key).subscribe(
+    //     res => {
+    //       this.moreThan20Logs[i].fields.worklog = res.json();
+    //       if (i === this.moreThan20Logs.length - 1) {
+    //         this.issues = [];
+    //         this.issues = this.issues.concat(this.moreThan20Logs).concat(lessThan21Logs);
+    //         console.log('issues', this.issues);
+    //         this.mapUserAndIssue(this.issues);
+    //       }
+    //     }
+    //   );
 
-          // }
-  //       }
-  //       else {
-  //         this.mapUserAndIssue(this.issues);
-  //       }
+    // }
+    //       }
+    //       else {
+    //         this.mapUserAndIssue(this.issues);
+    //       }
 
-  //     },
-  //     err => {
-  //       console.error('err', err);
-  //     }
-  //   );
-   }
+    //     },
+    //     err => {
+    //       console.error('err', err);
+    //     }
+    //   );
+  }
   // tickets with more than 20 logs
   // mapUserAndIssue(issues) {
   //   console.log('issues', issues);
@@ -156,13 +158,13 @@ export class AppComponent {
   //   return this.httpService.secureGet(url, this.username, this.password);
   // }
 
-  test(){
+  test() {
     let url = "http://localhost:8080/";
     let dataSend = this.userDataObj;
-     let encryptData = this.userDataObj.user+':'+this.password;
-     dataSend.encryptedData =  btoa(encryptData);
-        return this.httpService.securePost(url,dataSend, this.userDataObj.user, this.password).subscribe(res => {
-          console.log('res',res);
-        });
+    let encryptData = this.userDataObj.user + ':' + this.password;
+    dataSend.encryptedData = btoa(encryptData);
+    return this.httpService.securePost(url, dataSend, this.userDataObj.user, this.password).subscribe(res => {
+      console.log('res', res);
+    });
   }
 }
