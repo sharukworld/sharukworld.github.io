8e3187b1bd3f542cc806d6c790e1846bc08a98fc
sharuk ahmed mazumder <sharuk.ahmed@coda.global>
2017-06-19 00:07:04 +0530

    pushed

--- jira-shark/src/constant/url.ts
+++ jira-shark/src/constant/url.ts
@@ -1,7 +1,7 @@
 export class  Url {
 
 static get baseUrl(): string {
-    return (window.location.hostname ==='localhost')?'http://localhost:8080':'http://jira-service-jira-service-backup.7e14.starter-us-west-2.openshiftapps.com';
+    return (window.location.hostname ==='localhost')?'http://localhost:8080':'https://jira-service-jira-service-backup.7e14.starter-us-west-2.openshiftapps.com';
 }
 
 static get worklog(): string {
